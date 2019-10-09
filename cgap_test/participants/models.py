3ebbef4a816beca9e5e5c4987c167605b270ca88
Amanda Ruby <amanda.ruby@gmail.com>
2019-10-09 06:55:45 -0400

    Created basic Participant model

--- cgap_test/participants/models.py
+++ cgap_test/participants/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Participant(models.Model):
+    name = models.CharField()
+    age = models.IntegerField()
+    has_siblings = models.BooleanField()
