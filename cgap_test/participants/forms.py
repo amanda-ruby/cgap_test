cd4f36b3e301b2540b63c45b5a7a8aed1588a51e
Amanda Ruby <amanda.ruby@gmail.com>
2019-10-10 18:23:02 -0400

    Form saves data, no uniqueness checks

--- cgap_test/participants/forms.py
+++ cgap_test/participants/forms.py
@@ -1,8 +1,19 @@
 from django.forms import  ModelForm
 
+import json
+
 from . import models
 
 class ParticipantForm(ModelForm):
     class Meta:
         model = models.Participant
         exclude = ['review_status']
+
+    def make_json(self, raw_string):
+        return json.dumps([x.strip() for x in raw_string.split(',')])
+
+    def clean_exposures(self):
+        return self.make_json(self.cleaned_data['exposures'])
+
+    def clean_mutations(self):
+        return self.make_json(self.cleaned_data['mutations'])
--- cgap_test/participants/views.py
+++ cgap_test/participants/views.py
@@ -6,4 +6,9 @@ from . import forms
 # Create your views here.
 
 def participants_form(request):
-    return render(request, 'participants/participant_form.html', {'form': forms.ParticipantForm()})
+    if request.method == 'GET':
+        return render(request, 'participants/participant_form.html', {'form': forms.ParticipantForm()})
+    else:
+        f = forms.ParticipantForm(request.POST)
+        f.save()
+        return render(request, 'participants/participant_form.html', {'form': forms.ParticipantForm()})
