762a254fbfae9e2b3816950b471bcbaea12ffa96
Amanda Ruby <amanda.ruby@gmail.com>
2019-10-11 19:03:47 -0400

    on-submit redirect to list established

--- cgap_test/participants/urls.py
+++ cgap_test/participants/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.participants_form, name='participants_form')
+    url(r'^$', views.participants_form, name='participants_form'),
+    url(r'^list/', views.participants_list, name='participants_list')
 ]
--- cgap_test/participants/views.py
+++ cgap_test/participants/views.py
@@ -1,9 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from . import forms
 
-# Create your views here.
 
 def participants_form(request):
     if request.method == 'GET':
@@ -11,4 +9,7 @@ def participants_form(request):
     else:
         f = forms.ParticipantForm(request.POST)
         f.save()
-        return render(request, 'participants/participant_form.html', {'form': forms.ParticipantForm()})
+        return redirect('participants_list')
+
+def participants_list(request):
+    return render(request, 'participants/participant_list.html', {})
