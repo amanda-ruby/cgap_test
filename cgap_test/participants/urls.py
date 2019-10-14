2a7ed65e891a20633bea61cd749a56f90f55c131
Amanda Ruby <amanda.ruby@gmail.com>
2019-10-13 21:33:20 -0400

    Got ajax working on list view

--- cgap_test/participants/urls.py
+++ cgap_test/participants/urls.py
@@ -5,4 +5,4 @@ from . import views
 urlpatterns = [
     path(r'', views.participants_form, name='participants_form'),
     path(r'list/', views.participants_list, name='participants_list'),
-]
+    path(r'list/review/<int:id>/<str:rev_value>/', views.review_participant, name='review_participant')]
--- cgap_test/participants/views.py
+++ cgap_test/participants/views.py
@@ -1,5 +1,6 @@
 from django.forms import modelformset_factory
-from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 
 from . import forms
 from . import models
@@ -17,7 +18,17 @@ def participants_list(request):
     ParticipantFormSet = modelformset_factory(models.Participant,
                                    fields=['name', 'age', 'has_siblings', 'exposures', 'mutations', 'review_status'],
                                    labels={'name':'', 'age':'', 'has_siblings': '', 'exposures': '', 'mutations':'', 'review_status':''},
-                                   help_texts={'exposures': '', 'mutations': ''})
-    formset  = ParticipantFormSet()
-   
+                                   help_texts={'exposures': '', 'mutations': ''}
+            )
+    formset  = ParticipantFormSet()   
     return render(request, 'participants/participant_list.html', {'formset': formset})
+
+def review_participant(request, id, rev_value):
+    if request.method == 'POST':
+        print("Made it to view")
+        instance = get_object_or_404(models.Participant, id=id)
+        instance.review_status = rev_value
+        instance.save()
+        return HttpResponse("{great success}")
+    else:
+        return redirect('participants_list')
